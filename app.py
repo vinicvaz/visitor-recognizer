d114fa52d7e57fc6612119c4adc870816cd9b4d9
vinicvaz <vinicvaz95@gmail.com>
2020-04-16 19:16:58 -0300

    get data from socket and decode on backend

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, redirect, url_for, render_template
 from flask_socketio import SocketIO
+import base64
+from io import BytesIO
+import numpy as np
+from PIL import Image
+import io
+import cv2
 
 app = Flask(__name__, template_folder='templates')
 
@@ -15,11 +21,16 @@ socketio = SocketIO(app)
 def handle_message():
     return render_template("index.html")
 
-
+# Function to get data from socket and decode (have to add np array converter)
 @socketio.on('event')
 def handle_event(data):
-    image = data['data']
-    print(image)
+
+    data_splitted = data.split(',')[1]
+    data_encodded = data_splitted.encode()
+    data_decoded = base64.decodebytes(data_encodded)
+
+    with open('test.jpg', 'wb') as f:
+        f.write(data_decoded)
 
 
 if __name__ == "__main__":
